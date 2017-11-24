4272e19a200dacf9f73175a87ca893316e90bd9c
stopachka <stepan.p@gmail.com>
2017-11-24 23:37:40 +0000

    sm

--- magenta/3-train.sh
+++ magenta/3-train.sh
@@ -1,5 +1,5 @@
 polyphony_rnn_train \
---run_dir=/home/carnd/deep-dj/magenta/data/logdir/run2 \
+--run_dir=/home/carnd/deep-dj/magenta/data/logdir/run3 \
 --sequence_example_file=/home/carnd/deep-dj/magenta/data/sequence_examples/training_poly_tracks.tfrecord \
 --hparams="batch_size=64,rnn_layer_sizes=[64,64]" \
 --num_training_steps=20000
