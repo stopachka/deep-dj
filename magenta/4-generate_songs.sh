2cf2d2f41f3b2cdb117d2b0da5b5b93bf8f1a912
stopachka <stepan.p@gmail.com>
2017-11-24 23:26:25 +0000

    sm

--- magenta/4-generate_songs.sh
+++ magenta/4-generate_songs.sh
@@ -1,11 +1,12 @@
 polyphony_rnn_generate \
---run_dir=/home/carnd/deep-dj/magenta/data/logdir/run2 \
+--run_dir=/home/carnd/deep-dj/magenta/data/logdir/run1 \
 --output_dir=/home/carnd/deep-dj/magenta/generated \
 --num_outputs=10 \
 --num_steps=128 \
---primer_pitches="[67,64,60]" \
+--primer_midi=/home/carnd/deep-dj/magenta/data/midi/Bpm98_E_TokyoZapp_pad1.mid \
 --condition_on_primer=true \
---inject_primer_during_generation=false
+--inject_primer_during_generation=false \
+--hparams="batch_size=64,rnn_layer_sizes=[64,64]"
 
 
 # melody_rnn_generate \
