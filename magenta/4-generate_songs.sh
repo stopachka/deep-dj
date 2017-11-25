9322a1ae1263a608f241fdf32493e3a96c8d45ba
stopachka <stepan.p@gmail.com>
2017-11-25 00:45:42 +0000

    sm

--- magenta/4-generate_songs.sh
+++ magenta/4-generate_songs.sh
@@ -3,7 +3,7 @@ melody_rnn_generate \
 --run_dir=/home/carnd/deep-dj/magenta/data/logdir/run4 \
 --output_dir=/home/carnd/deep-dj/magenta/generated \
 --num_outputs=10 \
---num_steps=128 \
+--num_steps=50 \
 --hparams="batch_size=64,rnn_layer_sizes=[64,64]" \
 --primer_midi=/home/carnd/deep-dj/magenta/data/midi/highbpm/Bpm100_E_SuperSport_bass2.mid
 
